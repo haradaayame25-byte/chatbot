0db600bc0978cc9c928f5f897912c40c91a9358b
haradaayame25-byte <harada.ayame.25@gs.shizuoka.ac.jp>
2026-07-23 14:23:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from rag import ask_question
 print("大学チャットボット")
 print("終了するには exit を入力")
 
+#exitとなるまで繰り返す
 while True:
     question = input("あなた：")
 
+    #exitと入力されたとき
     if question.lower() == "exit":
         break
 
+    #AIの返答
     answer = ask_question(question)
     print("Bot：", answer)
